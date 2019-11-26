af1f4ede06f33f6ce662bcfbc1fdccd81be5c928
lpawelcz <czarnecki.l.p@gmail.com>
2019-11-26 01:33:48 +0100

    add more control over exceptions

--- ble-hub.py
+++ ble-hub.py
@@ -8,6 +8,8 @@ from datetime import datetime
 
 adapter = pygatt.backends.GATTToolBackend()
 
+cycle_time = 5
+
 temp_min = -4000
 temp_max = 8500
 temp_error_val = 0
@@ -50,7 +52,17 @@ def read_val(device, service_id, filename, min_val, max_val, prev_val, error_val
     while condition:
         timeout -= 1
         # Read characteristic
-        charac = device.char_read(service_id)
+        try:
+            charac = device.char_read(service_id)
+        except:
+            date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            condition = timeout
+            print("{}	Failed to read characteristic {}".format(date, service_id))
+            if not condition:
+                return -1
+            else:
+                continue
+
         int_val = int.from_bytes(charac, byteorder='little', signed=True)
         # Check if there is need for second characteristic readout
         condition = (not (int_val >= min_val and int_val <= max_val and int_val != error_val)) and timeout
@@ -75,58 +87,83 @@ def read_val(device, service_id, filename, min_val, max_val, prev_val, error_val
 
     return int_val
 
+def main():
+    try:
+        # write stdout also to log
+        tee = subprocess.Popen(["tee", dump_file], stdin=subprocess.PIPE)
+        os.dup2(tee.stdin.fileno(), sys.stdout.fileno())
+        os.dup2(tee.stdin.fileno(), sys.stderr.fileno())
 
-try:
-    # write stdout also to log
-    tee = subprocess.Popen(["tee", dump_file], stdin=subprocess.PIPE)
-    os.dup2(tee.stdin.fileno(), sys.stdout.fileno())
-    os.dup2(tee.stdin.fileno(), sys.stderr.fileno())
+        adapter.start()
 
-    adapter.start()
+        while True:
+            try:
+                date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                device = adapter.connect(node1_addr)
+                print("{}	Connected to {}".format(date, node1_addr))
+            except:
+                print("{}	Timeout when connecting to {}".format(date, node1_addr))
+                continue
 
-    device = adapter.connect(node1_addr)
-    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print("{}	Connected to {}".format(date, node1_addr))
+            temp_prev = 0
+            hum_prev = 0
+            press_prev = 0
+            uvi_prev = 0
 
-    temp_prev = 0
-    hum_prev = 0
-    press_prev = 0
-    uvi_prev = 0
+            # Read measurements from node
+            int_temp = read_val(device, temp_service, temp_file, temp_min, temp_max, temp_prev, temp_error_val)
+            if int_temp == -1:
+                return -1
+            temp_prev = int_temp
+            int_hum = read_val(device, hum_service, hum_file, hum_min, hum_max, hum_prev, hum_error_val)
+            if int_hum == -1:
+                return -1
+            hum_prev = int_hum
+            int_press = read_val(device, press_service, press_file, press_min, press_max, press_prev)
+            if int_press == -1:
+                return -1
+            press_prev = int_press
+            int_uvi = read_val(device, uvi_service, uvi_file, uvi_min, uvi_max, uvi_prev)
+            if int_uvi == -1:
+                return -1
+            uvi_prev = int_uvi
 
-    while True:
-        # Read measurements from node
-        int_temp = read_val(device, temp_service, temp_file, temp_min, temp_max, temp_prev, temp_error_val)
-        temp_prev = int_temp
-        int_hum = read_val(device, hum_service, hum_file, hum_min, hum_max, hum_prev, hum_error_val)
-        hum_prev = int_hum
-        int_press = read_val(device, press_service, press_file, press_min, press_max, press_prev)
-        press_prev = int_press
-        int_uvi = read_val(device, uvi_service, uvi_file, uvi_min, uvi_max, uvi_prev)
-        uvi_prev = int_uvi
+            date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            try:
+                device.disconnect()
+                print("{}	Disconnected from {}".format(date, node1_addr))
+            except:
+                print("{}	Already disconnected from {}".format(date, node1_addr))
 
-        # Convert measurements to correct format according to BT spec - characteristics
-        float_temp = float(int_temp) / 100;
-        float_hum = float(int_hum) / 100;
-        float_press = float(int_press) / 1000;
+            # Convert measurements to correct format according to BT spec - characteristics
+            float_temp = float(int_temp) / 100;
+            float_hum = float(int_hum) / 100;
+            float_press = float(int_press) / 1000;
 
-        date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print("{}	Temperature: {} deg C".format(date, float_temp))
-        print("{}	Humidity:    {} %".format(date, float_hum))
-        print("{}	Pressure:    {} hPa".format(date, float_press))
-        print("{}	UV Index:    {}".format(date, int_uvi))
+            date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print("{}	Temperature: {} deg C".format(date, float_temp))
+            print("{}	Humidity:    {} %".format(date, float_hum))
+            print("{}	Pressure:    {} hPa".format(date, float_press))
+            print("{}	UV Index:    {}".format(date, int_uvi))
 
-        # Prepare and send data to Domoticz
-        final_url = url + json + str(node1_temp) + nvalue + '0' + svalue + str(float_temp)
-        requests.get(final_url)
-        final_url = url + json + str(node1_hum) + nvalue + str(float_hum) + svalue + '0'
-        requests.get(final_url)
-        final_url = url + json + str(node1_press) + nvalue + '0' + svalue + str(float_press) + ';0'
-        requests.get(final_url)
-        final_url = url + json + str(node1_uv) + nvalue + '0' + svalue + str(int_uvi) + ';0'
-        requests.get(final_url)
+            # Prepare and send data to Domoticz
+            final_url = url + json + str(node1_temp) + nvalue + '0' + svalue + str(float_temp)
+            requests.get(final_url)
+            final_url = url + json + str(node1_hum) + nvalue + str(float_hum) + svalue + '0'
+            requests.get(final_url)
+            final_url = url + json + str(node1_press) + nvalue + '0' + svalue + str(float_press) + ';0'
+            requests.get(final_url)
+            final_url = url + json + str(node1_uv) + nvalue + '0' + svalue + str(int_uvi) + ';0'
+            requests.get(final_url)
 
-        time.sleep(5);
+            time.sleep(cycle_time);
 
-finally:
-    device.disconnect()
-    adapter.stop()
+    finally:
+        try:
+            device.disconnect()
+            print("{}	Disconnected from {}".format(date, node1_addr))
+        except:
+            print("{}	Already disconnected from {}".format(date, node1_addr))
+        adapter.stop()
+
+main()
